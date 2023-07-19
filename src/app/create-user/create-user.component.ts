3ee618f05b885e885584aa28f916866af8bebb65
vamshikumar123456789 <goudvamshikumar88@gmail.com>
2023-07-19 13:09:10 +0530

    cards added to form

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -18,10 +18,31 @@ public userForm:FormGroup = new FormGroup({
     hno:new FormControl(),
     state:new FormControl(),
     pin: new FormControl(),
-  })
-
+  }),
+  type: new FormControl(),
+  busfee: new FormControl(),
+  hostelfee: new FormControl(),
+  cards: new FormArray([]),
 
 });
+get cardsFormArray(){
+ return this.userForm.get('cards') as FormArray;
+}
+
+addCard(){
+
+  this.cardsFormArray.push(
+    new FormGroup({
+      no: new FormControl(),
+      exp: new FormControl(),
+      cvv:new FormControl(),
+    })
+  )
+}
+
+deletecard(i:number){
+  this.cardsFormArray.removeAt(i)
+}
 
 submit(){
   console.log(this.userForm);
