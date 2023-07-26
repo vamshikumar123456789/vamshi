f9254762cd0aecc9032bb8a7050790578903998e
vamshikumar123456789 <goudvamshikumar88@gmail.com>
2023-07-26 11:35:43 +0530

    parent to child ,child to parent communication

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent {
+@Input() public ac:number = 0;
+
+public bc:number = 0;
+@Output() public bEvent:EventEmitter<any> = new EventEmitter();
+
+send(){
+  this.bEvent.emit(this.bc)
+}
+}
